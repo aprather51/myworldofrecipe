cb4b1d90891317e1f3624ca1c9faa562b90b3b2b
Arthur Prather <apratherjr@gmail.com>
2019-04-18 21:18:21 -0500

    Updates

---
+++ precache-manifest.974416c10df7bd8d164d97fc397ede64.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "38d7684714525c4c6fed",
+    "url": "/my-world-of-recipe/static/js/runtime~main.38d76847.js"
+  },
+  {
+    "revision": "7246b87a0eda51885b0f",
+    "url": "/my-world-of-recipe/static/js/main.7246b87a.chunk.js"
+  },
+  {
+    "revision": "cad4d167ce837665f068",
+    "url": "/my-world-of-recipe/static/js/1.cad4d167.chunk.js"
+  },
+  {
+    "revision": "7246b87a0eda51885b0f",
+    "url": "/my-world-of-recipe/static/css/main.852ef260.chunk.css"
+  },
+  {
+    "revision": "c26e25bb6683af9ca6472f7829bc8df9",
+    "url": "/my-world-of-recipe/index.html"
+  }
+];
