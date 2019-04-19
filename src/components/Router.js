dfb687cfebde35cb2b9eaad173dd3bc5bafc9795
Arthur Prather <apratherjr@gmail.com>
2019-04-18 20:52:57 -0500

    no changes

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,23 +1,23 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 //Components
-import Header from './Header'
+import Header from './Header';
 import App from '../App';
 import Recipe from './Recipe';
 import Footer from './Footer';
 
 const Router = () => (
-  <>
-  <Header />
-	<BrowserRouter>
-		<Switch>
-			<Route exact path="/" component={App} />
-			<Route path="/recipe/:id" component={Recipe} />
-		</Switch>
-	</BrowserRouter>
-  <Footer />
-  </>
+	<Fragment>
+		<Header />
+		<BrowserRouter>
+			<Switch>
+				<Route exact path="/" component={App} />
+				<Route path="/recipe/:id" component={Recipe} />
+			</Switch>
+		</BrowserRouter>
+		<Footer />
+	</Fragment>
 );
 
 export default Router;
